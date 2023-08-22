ce036b4805d68a66e008c5949bd835d26dd2aa9d
Hyeonwoo Jung <wjdgusdn1223@gmail.com>
2023-08-23 00:22:16 +0900

    delete unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:currency_app/widgets/button.dart';
 import 'package:currency_app/widgets/currency_card.dart';
